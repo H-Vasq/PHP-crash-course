74e254d3e32b12585b14eb02ed1a4efb5797f2b7
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-07-05 18:14:52 +0000

    test and run concatenation

--- test.php
+++ test.php
@@ -1,8 +1,11 @@
 <?php
 
-    $name = 'Hello World';
+    $name = 'Hello World<br><br>';
+    $job = 'medic';
+    $myJob = 'My job is ' . $job . '<br><br>';
 
     echo $name;
+    echo $myJob;
 
 ?>
     
