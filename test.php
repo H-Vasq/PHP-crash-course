5914c7258e4d85380ab9f7707967376f6fc3ac4e
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-07-05 18:46:10 +0000

    use gettype

--- test.php
+++ test.php
@@ -3,7 +3,14 @@
     $name = 'Hello World<br><br>';
     $job = 'medic';
     $myJob = 'My job is ' . $job . '<br><br>';
-
+    $serial = 300;
+/*
     echo "$name" . "" ."" . "may name is Omar " . "" . "" . "" . "and my job is $job." . "<br><br>";
+*/
+
+    echo "The type of name is " . gettype($name) . "<br>";
+    echo "<br>";
+    echo "The type of serial is " . gettype($serial) . "<br><br>";
+
 ?>
     
