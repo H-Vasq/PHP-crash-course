292110a0e85e117699e165f670ea3d06f7e12c1b
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-07-06 18:12:00 +0000

    how to do associatvie arrays

--- test.php
+++ test.php
@@ -1,6 +1,10 @@
 <?php
 
-    $myArray = ['apple', 'banana', 'cherry', 5, 6, '2 boxes of fruit', '3 boxes of fruit', 7, 8, '9 boxes of fruit'];
+    $myArray = [
+        "stuff1" => ['apple', 'banana', 'cherry', 5, 6],
+        "stuff2" => ['2 boxes of fruit', '3 boxes of fruit', 7, 8],
+        "stuff3" => [ '9 boxes of fruit']
+    ];
 
     print_r($myArray);
 ?>
