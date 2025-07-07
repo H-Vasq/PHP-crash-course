409699fd68bf3809f8f0ee19cf885763cfe9d513
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-07-07 13:04:47 +0000

    test

--- test.php
+++ test.php
@@ -1,21 +1,8 @@
 <?php
 
-    $mecha = [
-        "name" => "Gundam",
-        "type" => "Mobile Suit",
-        "abilities" => ["Beam Rifle", "Shield", "Hyper Mode"],
-        "pilot" => [
-            "name" => "Amuro Ray",
-            "age" => 16,
-            "skills" => ["Piloting", "Combat", "Strategy"]
-        ]
-    ];
+    $array = ["hello", 30, "500 dollars"];
 
-    $num = 0;
-
-    foreach($mecha as $key => $val) {
-        // echo "$key. $val <br />";
-        print_r("$key. $val <br />");
-    }
+    print_r($array);
+   
 ?>
     
