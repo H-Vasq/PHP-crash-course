affd9c8a591530030d7ac271a18a1ae09ab507c9
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-07-08 23:30:05 +0000

    test switch

--- test.php
+++ test.php
@@ -1,27 +1,18 @@
 <?php
 
-    $deptA = [
-        "name" => "Tom",
-        "type" => "technician",
-        "serialID" => 0000333
-    ];
-   
-    $deptB = [
-        "name" => "Sara",
-        "type" => "medic",
-        "serialID" => 0000453
-    ];
+    $name = "Brand-";
 
-    $team01 = [$deptA, $deptB];
-
-    foreach($team01 as $tMember) {
-        
-        if($tMember['type'] == 'artist') {
-            echo "This is a" . " " . "Tech";
-        } else {
-            echo "We don't know who this is!";
-        };
+    switch($name) {
+        case 'Brand-A':
+            echo "Toyota";
+            break;
+        case 'Brand-B':
+            echo "Honda";
+            break;
+        default:
+            echo "Unknown";
+            break;
     }
-   
+
 ?>
     
