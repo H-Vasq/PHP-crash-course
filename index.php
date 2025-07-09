3548a71b709fdc5ddc1820bdc7409166cd67fb9e
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-07-09 15:36:04 +0000

    start constructor

--- index.php
+++ index.php
@@ -4,10 +4,15 @@
         public $name;
         public $type;
 
+        public function __construct() {
+            echo 'constructor ran...';
+        }
+
         public function greet() {
             return $this->name . ' This car is for SALE';
         }
     }
 
+    $user1 = new User();
     
 ?>
