2bf1931ea8ba676b114e7682b61ad2f948487df4
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-07-09 15:10:17 +0000

    start review

--- index.php
+++ index.php
@@ -1,17 +1,6 @@
-<form method="POST">
-    <label for="name">Name:</label>
-    <input type="text" name="name">
-    <br/>
-
-    <label for="age">Age</label>
-    <input type="number" name="age">
-    <br/>
-
-    <button type="submit">Submit</button>
-</form>
-
 <?php    
-if (isset($_POST['name'])) {
-    echo htmlspecialchars($_POST['name']) . " is " . (int)$_POST['age'] . " years old ";
-}
+
+    $name = "Hard Times";
+    echo $name;
+
 ?>
