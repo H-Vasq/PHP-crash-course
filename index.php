3d6e0dfe0d631ad209ba598c30cce5072a27c9f3
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-07-10 20:17:04 +0000

    test

--- index.php
+++ index.php
@@ -1,27 +1,7 @@
 <?php    
 
-    class User {
-        public $name;
-        public $type;
+    echo ("Hello there World");
 
-        public function __construct($name, $type) {
-            // echo 'constructor ran...';
-
-            $this->name = $name;
-            $this->type = $type;
-
-        }
-
-        public function greet() {
-            return $this->name . ' car is for SALE';
-        }
-    }
-
-    $user1 = new User('Honda', 'Sport');
-
-    echo $user1->name . " " . "is" . " " . "a" . " " . $user1->type . " " . "vehicle" . "." . "<br /><br />";
-    echo $user1->greet();
 ?>
 
-// start 
 
