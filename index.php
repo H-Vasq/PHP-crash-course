2e63428c7a44b9b98ec13d442766324bfbc43322
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-07-12 16:53:13 +0000

    start

--- index.php
+++ index.php
@@ -1,22 +1,13 @@
 <?php    
 
     class User {
-        public $name = 'Gord';
-        public function greet() {
-            return $this->name . ' says Hello !';
-        }
+       
     }
 
     $user1 = new User();
 
     $user2 = new User();
-    $user2->name = 'Susan';
-
-    echo '<br>';
-    echo $user2->greet();
-    echo '<br>';
-    echo '<br>';
-    echo $user1->greet();
+   
 
     
 
