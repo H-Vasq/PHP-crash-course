81d4b06859d4ec0c07c5943c343d45caabf1f13d
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-07-12 16:54:46 +0000

    start

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
 <?php    
 
     class User {
-       
+       public $name;
+       public $age;
+
+       public function __construct() {
+        echo 'constructor ran...';
+       }
+
+       public function greet(){
+        return $this->name . ' Hi, how are you?';
+       }
     }
 
     $user1 = new User();
