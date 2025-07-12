cc8b3844d61417bdae46558c0a3c04c4bd558f93
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-07-12 16:45:44 +0000

    test with $this and nested function

--- index.php
+++ index.php
@@ -2,12 +2,18 @@
 
     class User {
         public $name = 'Gord';
+        public function greet() {
+            return $this->name . ' says Hello !';
+        }
     }
 
     $user1 = new User();
 
-    echo $user1->name;
+    // echo $user1->name;
 
+    echo $user1->greet()
+
+    
     
 
 ?>
