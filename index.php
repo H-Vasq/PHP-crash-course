939f7ba37ab000899170945a47c12dd3710c76c2
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-07-14 01:59:37 +0000

    first test

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php    
-    
+    $create_printer = function() {
+        return function() {
+            echo "Hello functional!<br><br>";
+        };
+    };
+
+    $my_printer = $create_printer();
+    $my_printer();
 
 ?>
 
