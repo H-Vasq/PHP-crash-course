27193c01089e1ff532507cffd4a9a27f2f170062
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-07-14 02:06:40 +0000

    test and run

--- index.php
+++ index.php
@@ -8,5 +8,28 @@
     $my_printer = $create_printer();
     $my_printer();
 
+    // $double = function($x) {
+    //     return $x * 2;
+    // };
+
+    // $triple = function($x) {
+    //     return $x * 3;
+    // };
+
+    // $quadruple = function($x) {
+    //     return $x * 4;
+    // };
+
+    $create_multiplier = function($y) {
+        return function($x) use ($y) {
+            return $x * $y;
+        };
+    };
+
+    $double = $create_multiplier(2);
+    $triple = $create_multiplier(3);
+    $quadruple = $create_multiplier(4);
+    
+    echo $triple(13);
 ?>
 
