e791b241199489624ebcf5b0317ae8879d4b601e
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-07-14 00:57:06 +0000

    test and run

--- index.php
+++ index.php
@@ -17,13 +17,21 @@
        public function setName($name) {
         $this->name = $name;
        }
+
+       public function __get($property) {
+        if(property_exists($this, $property)) {
+            return $this->$property;
+        }
+       }
     }
 
     $user1 = new User('Joanna', 25);
 
-    echo $user1->setName('Roxanne');
-    echo $user1->getName();
+    // echo $user1->setName('Roxanne');
+    // echo $user1->getName();
 
+
+    echo $user1->__get('name');
 
 ?>
 
