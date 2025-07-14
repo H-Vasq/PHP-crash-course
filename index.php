f422304a5783d532ab786ed76dd31f7bd9f9950a
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-07-14 01:20:51 +0000

    start function as data

--- index.php
+++ index.php
@@ -4,11 +4,31 @@
         echo "Hello!";
     };
 
-    $my_function = function() {
-        echo "Hello! Sirs!\n";
+    $my_function = function($name) {
+        echo "Hello! $name!\n";
     };
 
-    $my_function();
+    $my_function("Roberta");
 
+    $fetch_data_real = function() {
+        echo "Fetching data...\n";
+    };
+
+    // $environment = 'dev';
+    $environment = 'prod';
+
+    $fetch_data_fake = function() {
+        return [
+            'name' => 'Jane',
+            'job' => 30,
+        ];
+    };
+
+    $fetch_data = ($environment === 'dev'
+    ? $fetch_data_fake
+    : $fetch_data_real);
+
+    // print_r($fetch_data());
+    $fetch_data();
 ?>
 
