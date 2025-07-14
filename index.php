6ff4b045ef3dc8db6c5bdee9b57cddd090a2d51a
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-07-14 01:58:04 +0000

    start lecture

--- index.php
+++ index.php
@@ -1,19 +1,5 @@
 <?php    
-
-    $add = function($x, $y) {
-        return $x + $y;
-    };
-
-    $subtract = function($x, $y) {
-        return $x - $y;
-    };
-
-    $combine_2_and_3 = function($func) {
-        return $func(2, 3);
-    };
-
-    echo $combine_2_and_3($add) . "\n<br><br>";
-    echo $combine_2_and_3($subtract) . "\n";
+    
 
 ?>
 
