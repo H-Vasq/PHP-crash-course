0c48d24efbaa5da2bd64167e39c7062ce49f1cf2
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-07-16 01:21:31 +0000

    start

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
-<?php    
-    $letters = ['a', 'b', 'c', 'd', 'e'];
-    $numbers = [1,2,3,4,5];
+<?php   
+    
 
-    $pairs = array_map(
-        fn($letter, $number) => "$letter$number",
-        $letters,
-        $numbers,
-    );
 
-    print_r($pairs);
 ?>
