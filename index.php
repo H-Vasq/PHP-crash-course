f7b8c1bc9018a4d4e73941f0ea7df1181f9e998d
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-07-16 01:39:53 +0000

    test and run

--- index.php
+++ index.php
@@ -3,7 +3,12 @@
     $numbers = [2,3,4,5,6,7,8,9,10];
 
     $get_sum = function($carry, $item) {
+        echo '$carry: ' . $carry . ', $item: ' . $item . "\n";
         return $carry + $item;
     };
+
+    $sum = array_reduce($numbers, $get_sum, 0);
+
+    echo "$sum\n";
 
 ?>
