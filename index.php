ca00d9d5cd57fcfa1b96ea2e7f16ba6bd335cd96
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-07-16 00:46:29 +0000

    splat operator

--- index.php
+++ index.php
@@ -1,11 +1,22 @@
 <?php    
 
-    $create_multiplier = function($y) {
-        return fn($x) => $x * $y;
-    };
-    
-    $double = $create_multiplier(2);
+    $names_1 = [
+        'john',
+        'betty',
+        'thomas',
+    ];
 
-    echo $double(18) . "\n";
+    $names_2 = [
+        'Peter',
+        'Diana',
+        'Carl',
+    ];
+
+    $all_names = [
+        ...$names_1,
+        ...$names_2,
+    ];
+
+    print_r($all_names);
 
 ?>
