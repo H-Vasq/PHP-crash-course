82626b1821548532741d1dc59001c918c6fa8e0b
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-07-16 01:35:59 +0000

    start

--- index.php
+++ index.php
@@ -1,38 +1,3 @@
 <?php
 
-    $employees = [
-        [
-            'name' => 'Bill',
-            'years_of_service' => 55,
-        ], [
-
-            'name' => 'Carol',
-            'years_of_service' => 34,
-        ], [
-            'name' => 'Clark',
-            'years_of_service' => 40,
-        ], [
-            'name' => 'Hannah',
-            'years_of_service' => 24,
-        ]
-    ];
-
-    function array_usort($array, $comparator_func) {
-        usort($array, $comparator_func);
-        return $array;
-    };
-
-    $years_of_service_comparator = function($a, $b) {
-        if ($a['years_of_service'] < $b['years_of_service']) {
-            return -1;
-        } else if ($a['years_of_service'] > $b['years_of_service']) {
-            return 1;
-        } else {
-            return 0;
-        }
-    };
-
-    $sorted_employees = array_usort($employees, $years_of_service_comparator);
-
-    print_r($sorted_employees);
 ?>
