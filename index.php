1530f6d8549f3b72ed987bd20c0c79255b778c58
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-07-16 00:40:41 +0000

    test and run

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php    
 
+    $create_multiplier = function($y) {
+        return fn($x) => $x * $y;
+    };
     
-    
+    $double = $create_multiplier(2);
+
+    echo $double(18) . "\n";
 
 ?>
