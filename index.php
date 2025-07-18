291893f9ed3ee5ab5ea658023015bf24464ca649
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-07-18 19:21:40 +0000

    test and run

--- index.php
+++ index.php
@@ -1,20 +1,24 @@
 <?php
     $employees = [
-    [
-        'name' => "John",
-        'age' => 43,
-        'salary' => 90000,
-        'job_title' => 'developer',
-    ], [
-        'name' => "Barnie",
-        'age' => 30,
-        'salary' => 50000,
-        'job_title' => 'admin',
-    ], [
-        'name' => "Korina",
-        'age' => 20,
-        'salary' => 4000,
-        'job_title' => 'junior',
-    ]
-];
+        [
+            'name' => "John",
+            'age' => 43,
+            'salary' => 90000,
+            'job_title' => 'developer',
+        ], [
+            'name' => "Barnie",
+            'age' => 30,
+            'salary' => 50000,
+            'job_title' => 'admin',
+        ], [
+            'name' => "Korina",
+            'age' => 20,
+            'salary' => 4000,
+            'job_title' => 'junior',
+        ]
+    ];
+
+    $developers = array_filter($employees, fn($x) => $x['job_title'] == 'developer');
+
+    print_r($developers);
 ?>
