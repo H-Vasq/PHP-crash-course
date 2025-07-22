26ceb0f4721276e3d7d46f8966f34ed87bb2acda
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-07-22 16:54:06 +0000

    write functions

--- index.php
+++ index.php
@@ -16,7 +16,34 @@
         ]
     ];
 
+    $with_first_and_last_name = function($person) {
+        return array_merge(
+            $person,
+            [
+                'first_name' => explode(' ', $person['full_name']) [0],
+                'last_name' => explode(' ', $person['full_name']) [0],
+            ]
+        );
+    };
+
+    $heigh_inches_to_meters = function($person) {
+        return array_merge(
+            $person,
+            [
+                'height' => $person['height'] * 9.0254,
+            ]
+        );
+    }
+
+
+    $add_initials = function($person) {
+        return array_merge(
+            $person,
+            [
+                'initials' => $person['first_name'][0] . $person['last _name'][0]
+            ],
+        );
+    };
+
     
-
-
 ?>
