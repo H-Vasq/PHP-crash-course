3519b4e192b383dbbe9a8a31abce6029d1e11966
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-07-22 16:26:04 +0000

    first test

--- index.php
+++ index.php
@@ -2,5 +2,10 @@
 
     $add = fn($x, $y, $z) => $x + $y + $z;
 
+    $add_partial = fn($x) => fn($y, $z) => $add($x, $y, $z);
 
+    $add_5 = $add_partial(5);
+    $sum = $add_5(6, 7);
+
+    echo $sum . "<br><br>";
 ?>
