8c25581cafb7e803d8c3426f2648af674a8d1385
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-07-22 16:21:40 +0000

    start

--- index.php
+++ index.php
@@ -1,41 +1,6 @@
 <?php
-    $employees = [
-        [
-            'name' => "John",
-            'age' => 43,
-            'salary' => 90000,
-            'job_title' => 'developer',
-        ], [
-        
-            'name' => "Minny",
-            'age' => 33,
-            'salary' => 1000,
-            'job_title' => 'developer',
-        ], [
-            'name' => "Barnie",
-            'age' => 30,
-            'salary' => 50000,
-            'job_title' => 'admin',
-        ], [
-            'name' => "Korina",
-            'age' => 20,
-            'salary' => 4000,
-            'job_title' => 'junior',
-        ]
-    ];
 
-    $developers = array_filter($employees, fn($x) => $x['job_title'] == 'developer');
+    $add = fn($x, $y, $z) => $x + $y + $z;
 
-
-    $developer_salaries = array_map(fn($x) => $x['salary'], $developers );
-
-    $total_developer_salaries = array_reduce(
-        $developer_salaries,
-        fn($carry, $item) => $carry + $item,
-    );
-
-    $average_developer_salary = $total_developer_salaries / count($developer_salaries);
-
-    echo "Average dev salary is $average_developer_salary <br><br>";
 
 ?>
