794581d0f40b6b63c6208fb9fd396ddbd9727789
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-07-28 18:01:34 +0000

    test with html

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
-<?php
+<html>
 
-echo 'Hello World!';
+<head>
+<title>Check</title>
+</head>
 
-?>
+<body>
+    <h1>A New World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et soluta odio error consequatur nesciunt, laborum dicta dolores optio. Deserunt ullam, excepturi veritatis perferendis assumenda saepe quaerat vero eius sunt tempora?</p><br>
+    <p><?php echo 'Does this work'; ?></p><br>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, nobis?</p>
+</body>
+
+
+</html>
