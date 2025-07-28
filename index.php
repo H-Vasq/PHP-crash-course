5a1b64158282076dd02f1c2defe5a402cc118dda
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-07-28 18:41:43 +0000

    check moving sentence to another line

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 
 $name = 'Mike';
+echo 'Hello, ' . $name . "\n<br>";
 
-echo "Joe's nickname was \"The Knife\".";
+echo "Joe's nickname was \"The Knife\".<br>";
+echo "Moe's nickname was \"The Knife\".";
 
 ?>
