26d6c0ecaf74e0efc7d0755f1922f5b6f8836fc7
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-07-28 18:05:14 +0000

    start lecture

--- index.php
+++ index.php
@@ -1,15 +1,8 @@
-<html>
+<?php
 
-<head>
-<title>Check</title>
-</head>
+$age = 40;
 
-<body>
-    <h1>A New World</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et soluta odio error consequatur nesciunt, laborum dicta dolores optio. Deserunt ullam, excepturi veritatis perferendis assumenda saepe quaerat vero eius sunt tempora?</p><br>
-    <p><?php echo 'Does this work'; ?></p><br>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, nobis?</p>
-</body>
+$double_age = $age * 8;
+echo $double_age;
 
-
-</html>
+?>
