5bf668f84d1898404abb84c0d2ea0dfe1c8f3029
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-07-30 18:24:56 +0000

    check and run

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
 
+$turtle = 'raph';
+$bandana = '';
 
+switch ($turtle) {
+    case 'leo':
+        $bandana = 'blue';
+    case 'raph':
+        $bandana = 'red';
+    case 'don':
+        $bandana = 'purple';
+    default:
+        $bandana = 'yellow';
+    }
+
+echo "$turtle's bandana is $bandana! <br>";
 
 ?>
