bccabb3be0a36a0f752043c4f0851ae081aa9c1d
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-07-30 18:49:30 +0000

    test

--- index.php
+++ index.php
@@ -1,19 +1,7 @@
 <?php
 
-// $a = 12;
-
-// if ( $a % 2 == 0 ) {
-//     echo "$a is even";
-// } else {
-//     echo "$a is odd";
-// };
-
-$a = 5;
-$b = 7;
-
-echo 'The value of $a is ' . ++$b . "<br>";
-
-echo $a;
+$a = 'Okay';
+echo 'the value of $a is ' . ++$a;
 
 
 ?>
