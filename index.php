091eff36d2cb73a1bd2f28ca4379f689d7977b90
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-08-03 01:57:46 +0000

    test use case

--- index.php
+++ index.php
@@ -1,24 +1,9 @@
 <?php
 
-$colors = ['red', 'green', 'blue', 'yellow'];
+$message = "HELLO THERE";
 
-for ($i = 0; $i < sizeof($colors); $i++) {
-    echo $colors[$i] . "<br>";
-}
+$newmsg = strtolower($message);
 
-foreach( $colors as $color ) {
-    echo "$color <br>";
-}
-
-$home_towns = [
-    'Joe' => 'Echo, NY',
-    'Erma' => 'Charlie, AZ',
-    'Oscar' => 'Lima, PA',
-    'Ynez' => 'Delta, OR',
-];
-
-foreach( $home_towns as $name => $town) {
-    echo "$name lives in $town <br>";
-}
+echo $newmsg;
 
 ?>
