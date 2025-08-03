db62d943cdff74cf90a6f3929c01431693dd46bd
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-08-03 02:15:06 +0000

    start

--- index.php
+++ index.php
@@ -1,33 +1,5 @@
 <?php
 
-// function add_two($a) {
-//     return $a + 2;
-// }
-
-// $a = 2;
-// echo add_two($a);
-
-
-// function double(&$b)
-
-// {
-//     $b *= 2;
-// }
-
-// $b = 9;
-// double($b);
-
-// echo "<br> $b <br>";
-
-function triple() {
-    global $c;
-    $c *= 3;
-}
-
-$c = 10;
-triple();
-
-echo "<br> $c <br>";
 
 
 ?>
