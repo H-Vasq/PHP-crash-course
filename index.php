c6018e60abc3c6e04dca7074f840cc16ab912223
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-08-03 01:49:59 +0000

    associative arrary foreach test

--- index.php
+++ index.php
@@ -10,4 +10,15 @@ foreach( $colors as $color ) {
     echo "$color <br>";
 }
 
+$home_towns = [
+    'Joe' => 'Echo, NY',
+    'Erma' => 'Charlie, AZ',
+    'Oscar' => 'Lima, PA',
+    'Ynez' => 'Delta, OR',
+];
+
+foreach( $home_towns as $name => $town) {
+    echo "$name lives in $town <br>";
+}
+
 ?>
