6c346f850a30765144e52b4dc8fd7c65952145c6
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-08-03 01:46:33 +0000

    test case

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 
+$colors = ['red', 'green', 'blue', 'yellow'];
 
+for ($i = 0; $i < sizeof($colors); $i++) {
+    echo $colors[$i] . "<br>";
+}
+
+foreach( $colors as $color ) {
+    echo "$color <br>";
+}
 
 ?>
