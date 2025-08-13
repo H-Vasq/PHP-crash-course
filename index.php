ce8d970363d79f3d9d2e8b8e0dea57f17f650596
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-08-13 17:12:23 +0000

    start

--- index.php
+++ index.php
@@ -1,41 +1,5 @@
 <?php
 
-class Person {
-    var $first_name;
-    var $age;
-    var $birthday = false;
 
-    function __construct( $first_name, $age )
-    {
-        $this->first_name = $first_name;
-        $this->age = $age;
-    }
-
-    public function get_first_name() {
-        return $this->first_name;
-    }
-
-    public function get_age() {
-        return $this->age;
-    }
-
-    public function set_birthday( $b ) {
-        $this->birthday = $b;
-        $this->update_age();
-    }
-
-    private function update_age() {
-        $this->age = ($this->birthday) ? ++$this->age : $this->age;
-    }
-}
-
-$Minerva = new Person( 'Minerva', 35);
-$Alice = new Person( 'Ginny', 45);
-
-echo $Minerva->get_first_name(). "<br>";
-
-$Alice->set_birthday(true);
-
-echo "Alice is " . $Alice->get_age() . ".<br>";
 
 ?>
